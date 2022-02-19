ff561e2a4639a60ef823d27a63c7d8c678d92f71
cfabian <cfabian@student.42wolfsburg.de>
2022-02-19 13:20:05 +0100

    get timestamp

--- src/time.c
+++ src/time.c
@@ -6,7 +6,18 @@
 /*   By: cfabian <cfabian@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/19 12:27:39 by cfabian           #+#    #+#             */
-/*   Updated: 2022/02/19 12:27:40 by cfabian          ###   ########.fr       */
+/*   Updated: 2022/02/19 13:17:26 by cfabian          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../philo.h"
+
+int64_t	get_time_stamp(void)
+{
+	struct timeval	tv;
+	int64_t			now;
+
+	gettimeofday(&tv, NULL);
+	now = tv.tv_sec * 1000 + tv.tv_usec / 1000;
+	return (now);
+}
